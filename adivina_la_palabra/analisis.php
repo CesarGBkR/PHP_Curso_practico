ef56f65b62d639ae8e3b4a8578b5226f626021d8
CesarGBkR <yexotortuesprin@gmail.com>
2022-08-02 14:49:24 -0500

    Adivina la palabra juego v1.22

--- adivina_la_palabra/analisis.php
+++ adivina_la_palabra/analisis.php
@@ -4,8 +4,8 @@
     $palabras = array("sol", "luna", "cielo", "estrella", "lluvia");
     for ($i=0; $i < count($palabras) ; $i++) { 
         if ($_REQUEST[$i] == $palabras[$i]) {
-            echo "La palabra ingresada es correcta \n";
+            echo "La palabra ingresada es correcta"."<br>";
         }
         else
-            echo "La palabra ingresada es incorrecta, la palabra correcta es $palabras[$i] \n";
+            echo "La palabra ingresada es incorrecta, la palabra correcta es $palabras[$i]"."<br>";
     }
